0e31308fad953a60f5d155b081d2c93ec8ce266f
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-05-09 16:27:14 +0100

    Counter completed
    Counter completed
    -> increase, decrease or reset the count
    -> count above 0: green
    -> count below 0: red
    -> count 0: black

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,41 @@
+// set up initial count
+let count = 0;
+
+// get Elements
+const value = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
+
+
+// add EventListeners
+buttons.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    const styles = e.target.classList;
+    // check button type
+    if(styles.contains('decrease')){
+      count--;
+    };
+    if(styles.contains('reset')){
+      count = 0;
+    };
+    if(styles.contains('increase')){
+      count++;
+    };
+
+    // check count (over 0, equal to 0 or above 0)
+    if(count > 0){
+      value.classList.remove('red-count');
+      value.classList.add('green-count');
+    };
+    if(count < 0){
+      value.classList.remove('green-count');
+      value.classList.add('red-count');
+    };
+    if(count === 0){
+      value.classList.remove('green-count');
+      value.classList.remove('red-count');
+    };
+
+    value.textContent = count;
+
+  });
+});
