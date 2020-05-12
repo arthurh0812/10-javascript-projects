4e544cc614516e2bd17f233516cb4ca829c0308b
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-05-12 09:25:06 +0100

    Questions completed
    questions completed:
    -> questions with question title and plus icon to show the answer
    -> if plus icon is pressed minus icon appears to close the question answer again

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,29 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    const button = question.querySelector('.question-btn');
+    button.addEventListener('click', function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove('show-text');
+            };
+        });
+        question.classList.toggle('show-text');
+    });
+});
+
+
+
+
 // traversing the dom
+
+// const buttons = document.querySelectorAll('.question-btn');
+
+// buttons.forEach(function(button){
+//     button.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
