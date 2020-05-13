9909b5dc529f4d74a05b55c2f336ff2bb98ddcfd
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-05-13 11:29:57 +0100

    changes

--- 9-video/setup/app.js
+++ 9-video/setup/app.js
@@ -1,3 +1,17 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+// elements of sidebar
+const toggleButton = document.querySelector('.sidebar-toggle');
+const closeButton = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+// EventListeners of sidebar
+toggleButton.addEventListener('click', function(){
+    sidebar.classList.toggle('show-sidebar');
+});
+
+closeButton.addEventListener('click', function(){
+    sidebar.classList.remove('show-sidebar');
+});
